974dc2ee114e7cbfb039f4a728d08eb728ea9060
sahiltcg <sahilahmedtcg@gmail.com>
2025-01-17 22:01:41 +0530

    Login desing

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submithandler = (e) => {
+        e.preventDefault()
+        console.log("Hello guys form submitted");
+        
+    }
+
+
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
         <div className="border-2 border-emerald-600 rounded-xl p-20">
-            <form className='flex flex-col items-center justify-center gap-8 border-2'>
-                <input required className='text-white font-semibold outline-none bg-transparent border-2 border-emerald-600 px-5 py-4 text-xl rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email'/>
+            <form 
+            onSubmit={(e)=>{
+                    submithandler(e)
+            }} className='flex flex-col items-center justify-center gap-4'>
+                <input value={email}
+                onChange={(e)=>{
+                    setEmail(e.target.value)
+                    
+                }} required className='text-white font-semibold outline-none bg-transparent border-2 border-emerald-600 px-5 py-4 text-xl rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email'/>
                 <input required className='text-white font-semibold outline-none bg-transparent border-2 border-emerald-600 px-5 py-4 text-xl rounded-full placeholder:text-gray-400' type="password" placeholder='Enter password'/>
-                <button className='text-white font-semibold outline-none bg-transparent bg-emerald-600 px-10 py-5 text-xl rounded-full placeholder:text-orange-400'>Log in</button>
+                <button className='text-white font-semibold outline-none bg-transparent bg-emerald-600 hover:bg-emerald-700 px-32 py-3 text-lg rounded-full mt-10 placeholder:text-orange-400'>Log in</button>
             </form>
         </div>
     </div>
