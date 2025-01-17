f4aece760163ffbeb0fe06c266c2479d0ff12a9f
sahiltcg <sahilahmedtcg@gmail.com>
2025-01-17 22:03:11 +0530

    Login desing

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
                     
                 }} required className='text-white font-semibold outline-none bg-transparent border-2 border-emerald-600 px-5 py-4 text-xl rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email'/>
                 <input required className='text-white font-semibold outline-none bg-transparent border-2 border-emerald-600 px-5 py-4 text-xl rounded-full placeholder:text-gray-400' type="password" placeholder='Enter password'/>
-                <button className='text-white font-semibold outline-none bg-transparent bg-emerald-600 hover:bg-emerald-700 px-32 py-3 text-lg rounded-full mt-10 placeholder:text-orange-400'>Log in</button>
+                <button className='text-white font-semibold outline-none bg-emerald-600 hover:bg-emerald-700 px-32 py-3 text-lg rounded-full mt-10 placeholder:text-orange-400'>Log in</button>
             </form>
         </div>
     </div>
